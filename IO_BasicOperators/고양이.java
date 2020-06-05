aa51dacfc94b3211abae1f0cbbc6a6805fd5e946
jsh4646 <62202441+jsh4646@users.noreply.github.com>
2020-06-05 10:50:54 +0900

    Add files via upload

---
+++ IO_BasicOperators/고양이.java
@@ -0,0 +1,9 @@
+public class 고양이 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n )  ( ')\n(  /  )\n \\(__)|");
+
+        for(int i = 0; i < 5; ++i) {
+            System.out.println(i);
+        }
+    }
+}
